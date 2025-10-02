2e2b24e1b621eaf536422b384f91ec9449f39054
Aman Verma <aman.angular13@gmail.com>
2025-10-02 19:57:46 +0530

    setup eureka server and config server - refector

--- pricing-service/src/main/java/com/amanverma/hotelmanagementsystem/pricing_service/PricingServiceApplication.java
+++ pricing-service/src/main/java/com/amanverma/hotelmanagementsystem/pricing_service/PricingServiceApplication.java
@@ -2,8 +2,10 @@ package com.amanverma.hotelmanagementsystem.pricing_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class PricingServiceApplication {
 
 	public static void main(String[] args) {
